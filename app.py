88857e065eca8b73626cb7ad76c3e72371f582b0
Divya Pandey <divyap036@gmail.com>
2026-06-28 15:44:29 +0530

    changes in app file

--- app.py
+++ app.py
@@ -11,16 +11,30 @@ import string
                      
 ps = PorterStemmer() 
 
+//new lines
 
-nltk_data_dir = "/opt/render/nltk_data"
+
+
+nltk_data_dir = os.path.join(os.path.dirname(__file__), "nltk_data")
 os.makedirs(nltk_data_dir, exist_ok=True)
-
 nltk.data.path.append(nltk_data_dir)
 
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger', download_dir= nltk_data_dir)
-nltk.download('punkt',download_dir= nltk_data_dir)
-nltk.download('punkt_tag', download_dir=nltk_data_dir)
+nltk.download('punkt', download_dir=nltk_data_dir)
+nltk.download('stopwords', download_dir=nltk_data_dir)
+
+
+
+
+
+#nltk_data_dir = "/opt/render/nltk_data"
+#os.makedirs(nltk_data_dir, exist_ok=True)
+
+#nltk.data.path.append(nltk_data_dir)
+
+#nltk.download('punkt')
+#nltk.download('averaged_perceptron_tagger', download_dir= nltk_data_dir)
+#nltk.download('punkt',download_dir= nltk_data_dir)
+#nltk.download('punkt_tag', download_dir=nltk_data_dir)
 
 #Text Vectorization
 tfIdf = pickle.load(open('vectorizer.pkl','rb'))
